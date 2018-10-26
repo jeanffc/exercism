ad54731e58c32eea6a17176c60301ed8b61b7adc
Jean Campos <jeanffc@gmail.com>
2018-10-26 18:50:40 -0300

    bob

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -2,9 +2,9 @@ class Bob
   def self.hey (str)
     if str.strip.empty?
       'Fine. Be that way!'
-    elsif str.end_with?('?') && str.upcase == str
+    elsif /[A-Z]/ =~ str && str.upcase == str && str.end_with?('?')
       "Calm down, I know what I'm doing!"
-    elsif /[a-zA-Z]+/ =~ str && str.upcase == str
+    elsif /[A-Z]/ =~ str && str.upcase == str
       'Whoa, chill out!'
     elsif str.strip.end_with?('?')
       'Sure.'
